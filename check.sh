3c9de609eddf9144972525d5ca5c470543a757cf
rubo77 <github@r.z11.de>
2018-06-28 23:16:57 +0200

    check.sh: change to nord meshviewer

--- check.sh
+++ check.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-curl -s https://mapdata.freifunk-vogtland.net/meshviewer.json -o meshviewer.json
+curl -s https://mesh.freifunknord.de/data/meshviewer.json -o meshviewer.json
 ./site-merge-detect.py meshviewer.json || {
     cp meshviewer.json "meshviewer_$(date '+%s').json"
 }
